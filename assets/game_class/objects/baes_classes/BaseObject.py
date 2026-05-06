8b7177fb94d2a8f3de2d5743fd87577b75fdd935
ZhiBo Yin <zyin0414@uni.sydney.edu.au>
2026-05-06 20:45:48 +0800

    todo: >> add boxbase class >> add rectangular box class >> add circal box class

--- assets/game_class/objects/baes_classes/BaseObject.py
+++ assets/game_class/objects/baes_classes/BaseObject.py
@@ -1,14 +1,24 @@
-from Location import Location
+from assets.game_class.objects.geometry.Location import Location
+from typing import Optional,Generator
 class BaseObject():
+    
+    @staticmethod
+    def gen_unicode() -> Generator[str,None,None]:
+        number = 0
+        while True:
+            yield f"{number:.12d}"
+            number += 1
+    unicode_generator = gen_unicode()
+
     """
     object_unique_code is the unique string to represent this object
     object_id is the id of this object, it is designed to be the same for the same 'leaf class'
     location is the location of this object
     """
-    def __init__(self, object_unicode: str, object_id: str, location: Location=Location()):
-        self._object_unicode:str = object_unicode
+    def __init__(self, object_unicode: str, object_id: str, location: Optional[Location]=None):
+        self._object_unicode:str = next(BaseObject.unicode_generator)
         self.object_id = object_id
-        self.location:Location = location
+        self.location:Location = Location() if location is None else location
         
 
     def __eq__(self,o):
